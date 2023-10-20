7a8dd8daa36d39f8a9919f462f5f3740fda44dab
zfriedman0 <friedman1080@outlook.com>
2023-10-19 23:45:10 -0500

    Build and deploy.

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+const { DateTime } = require('luxon');
+
+module.exports = function (eleventyConfig) {
+    // Copy the 'css' directory to the output (_site folder)
+    eleventyConfig.addPassthroughCopy('/src/css');
+    
+    // Watch the 'css' directory for changes
+    eleventyConfig.addWatchTarget('/src/css');
+
+    eleventyConfig.addFilter('readableDate', (dateObj) => {
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
+            'dd LLL yyyy'
+        );
+    }); 
+
+    return {
+        dir: {
+            input: "src",
+            output: "public",
+        },
+    };
+};
