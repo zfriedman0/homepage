1f356b21f34676f0c271af07268cf826a98ca353
zfriedman0 <friedman1080@outlook.com>
2024-12-03 15:40:00 -0600

    Don't ignore _site.

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,9 @@ module.exports = function (eleventyConfig) {
     //eleventyConfig.setBrowserSyncConfig({
     //    files: '_site/css/**/*.css'
     //});
+
+    // Copy `img/favicon/` to `_site/`
+    eleventyConfig.addPassthroughCopy({ "src/img/favicon.ico": "/" });
     
     // Watch the 'css' directory for changes
     eleventyConfig.addWatchTarget('src/sass');
